c12f8710b120c9d5ac074fa949a9e8d85394f941
Darina837 <daryna.koloda@nure.ua>
2019-11-13 22:49:43 +0200

    Updates

---
+++ precache-manifest.8d7ba774f81c9a07fb717de4eb095568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca960b59e832ca66141cb425975d7ed",
+    "url": "/REACT_hw1/index.html"
+  },
+  {
+    "revision": "941b1dee20c0cdf05c8c",
+    "url": "/REACT_hw1/static/css/main.0751fed2.chunk.css"
+  },
+  {
+    "revision": "d389cdad375d6cb320b5",
+    "url": "/REACT_hw1/static/js/2.de025bca.chunk.js"
+  },
+  {
+    "revision": "941b1dee20c0cdf05c8c",
+    "url": "/REACT_hw1/static/js/main.531e58c1.chunk.js"
+  },
+  {
+    "revision": "e30821750c9ed1f57f77",
+    "url": "/REACT_hw1/static/js/runtime-main.9b324d8e.js"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "/REACT_hw1/static/media/252.e45d9d1f.png"
+  }
+]);
